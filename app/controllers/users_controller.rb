8cc06b85fc1409f4a33e725626c17ddd69a610f2
Bobby Hinton <bob.g.hint@gmail.com>
2023-04-23 22:08:45 -0500

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::InvalidRecord, with: :handle_invalid_data
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  def show
+    user = User.find_by(id: session{:user_id})
+    
+    if user
+      render json: user, status: :created
+    else
+      render json: {error: "Not Authorized" }, status: :unauthorized
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :name, :email, :preferred_craft, :level_of_skill)
+  end
+
+  def handle_invalid_data(invalid)
+    render json: {errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
 import { useEffect, useState, createContext, useContext } from 'react';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loginToggle, setLoginToggle] = useState(false);
 
-  
+  useEffect(()=> {
+    fetch("/me")
+    .then((response) => {
+      if(response.ok)
+      {
+        response.json()
+        .then(user => setUser(user))
+      }
+    })
+  }, [])
 
   return (
     <div>
